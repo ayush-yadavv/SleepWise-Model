aa9271bc6e95f3d6f91d06f233056d3a68129f0f
ayush <yadav.ayushx1@gmail.com>
2025-06-09 22:56:47 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from keras.models import load_model
+import numpy as np
+
+app = Flask(__name__)
+model = load_model('best_sleep_model.h5')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    input_data = np.array(data['input']).reshape(1, -1)
+    prediction = model.predict(input_data)
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
